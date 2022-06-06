d31edbda0795c907dc50ad3c18fbf3f6ebc6bd12
Ben Cooper <ben.f.cooper@gmail.com>
2022-06-06 11:45:30 +0100

    feat(SERVER): HTTP/2 trial

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const port = process.env.PORT || 3000;
-const app = express();
+const express = require('express')
+const http2Express = require('http2-express-bridge')
+const http2 = require('http2')
+const { readFileSync } = require('fs')
+// only change required
+const app = http2Express(express)
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const http2 = require('http2')
+const options = {
+    // Heroku requires the certificate to be in a file
+    // Get the certificate from the Heroku dashboard
 
-// create a new server instance
-const server = http2.createServer({}, app).listen(port, () => {
-  console.log(`server started port: ${port}`);
+    key: readFileSync('./cert/server.key'),
+    cert: readFileSync('./cert/server.crt'),
+    allowHTTP1: true
+}
+const server = http2.createSecureServer(options, app)
+server.listen(port).on('listening', () => {
+    console.log(`Listening on port ${port}`)
 });
-
-server.on('error', (err) => {
-  console.error(err);
-});
-
 
