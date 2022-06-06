87c34e7a6d8ea9fe47dc18e1527e69e35f0a3a8f
Ben Cooper <ben.f.cooper@gmail.com>
2022-06-06 11:51:39 +0100

    fix(SERVER): remove HTTP/2

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-const path = require('path');
-const fs = require('fs');
-const port = process.env.PORT || 3000;
-const express = require('express')
-const http2Express = require('http2-express-bridge')
-const http2 = require('http2')
-const { readFileSync } = require('fs')
-// only change required
-const app = http2Express(express)
+var express = require('express');
+var path = require('path');
+
+var port = process.env.PORT || 3000;
+var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-const options = {
-    allowHTTP1: true
-}
-const server = http2.createSecureServer(options, app)
-server.listen(port).on('listening', () => {
-    console.log(`Listening on port ${port}`)
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
 });
-
