1c02d69fdfd6588c2ca4dd71c6b87ee84a8a4e65
Ben Cooper <ben.f.cooper@gmail.com>
2022-06-07 09:56:42 +0100

    feat(UI): added time

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 //instantiate MDC components
 const ripple = new mdc.ripple.MDCRipple(document.querySelector('.mdc-button'));
+
+const clock = setInterval(() => {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let time = `${hours}:${minutes}:${seconds}`;
+    document.querySelector('.time').innerHTML = time;
+}, 1000);
