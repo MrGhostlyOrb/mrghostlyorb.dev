57b84d8f5ddff0b25e890b21e79c7b425d847ddc
Ben Cooper <ben.f.cooper@gmail.com>
2022-08-21 09:12:37 +0100

    feat(SERV): Added SSL

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const http2 = require('http2');
+const fs = require("fs");
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -10,10 +12,11 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(`server started port: ${port}`);
-  }
-});
+http2.createSecureServer({
+  key: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/fullchain.pem')
+}, app).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+
+}
+);
