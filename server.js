ebcf6a053649dfd49fe679b4e6daa4b127294fea
Ben Cooper <ben.f.cooper@gmail.com>
2022-08-21 09:35:34 +0100

    fix(SERV): Change to spdy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http2 = require('http2');
 const fs = require("fs");
+const spdy = require('spdy');
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -12,11 +13,12 @@ app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-http2.createSecureServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/fullchain.pem')
-}, app).listen(port, () => {
-  console.log(`Server running on port ${port}`);
-
-}
-);
+spdy.createServer({
+  key: fs.readFileSync('localhost-privkey.pem'),
+  cert: fs.readFileSync('localhost-cert.pem')
+}, app).listen(port, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
+});
