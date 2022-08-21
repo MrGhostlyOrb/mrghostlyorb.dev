0aaa789d9f17773ad6d0df211821919c2d9d5dfa
Ben Cooper <ben.f.cooper@gmail.com>
2022-08-21 10:08:11 +0100

    fix(SERV): Change to node-static and http/2

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const path = require('path');
 const fs = require("fs");
 const spdy = require('spdy');
 const nstatic = require('node-static');
+const http2 = require("http2");
 
 const port = process.env.PORT || 443;
 
 // Serve the static files from the dist directory
 const fileServer = new nstatic.Server(path.join(__dirname, 'dist'));
 
-const app = spdy.createServer({
+http2.createSecureServer({
   key: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/mrghostlyorb.dev/fullchain.pem'),
   allowHTTP1: true
